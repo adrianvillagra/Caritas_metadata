ae6b82a503393a5217a8c0f58a69d79efeb741b7
AdrianVillagra <adrian_enry_xxv@hotmail.com>
2022-02-22 16:50:46 -0300

    fix typo

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ import express from 'express';
 import cors from 'cors';
 import db from './database/db.js';
 import productRoutes from './routes/ProductRoutes.js';
+import calendarRoutes from './routes/CalendarRoutes.js';
+import mesuareRoutes from './routes/MesuareRoutes.js';
+import orderDetailRoutes from './routes/OrderDetailRoutes.js';
+import recipeDetailRoutes from './routes/RecipeDetailRoutes.js';
+import recipeRoutes from './routes/RecipeRoutes.js';
+import stockRoutes from './routes/StockRoutes.js';
+import supplierRoutes from './routes/SupplierRoutes.js';
+import typeRoutes from './routes/TypeRoutes.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/products', productRoutes);
+app.use('/calendars', calendarRoutes);
+app.use('/mesuares', mesuareRoutes);
+app.use('/order_details', orderDetailRoutes);
+app.use('/recipe_details', recipeDetailRoutes);
+app.use('/recipe', recipeRoutes);
+app.use('/stock', stockRoutes);
+app.use('/suppliers', supplierRoutes);
+app.use('/types', typeRoutes);
 
 try {
 	await db.authenticate();
