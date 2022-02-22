19b17fa92bbdc4cacd87324939c00d04d7f09cfa
AdrianVillagra <adrian_enry_xxv@hotmail.com>
2022-02-22 17:03:13 -0300

    fix colum in type

--- models/TypeModel.js
+++ models/TypeModel.js
@@ -2,7 +2,6 @@ import db from '../database/db.js';
 import { DataTypes } from 'sequelize';
 
 const ProductModel = db.define('types', {
-	mesuare_id: { type: DataTypes.INTEGER },
 	name: { type: DataTypes.STRING },
 });
 
